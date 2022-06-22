faef1d8a15428134fd4e1f4289382d6e8146c508
sashakhalikov <sasha.khalikov@gmail.com>
2022-06-22 12:08:10 +0300

    requirements.txt was added. Now it's not requesting API every time but writes API response to a local static/StormGlassData

--- src/Python_surf_project.py
+++ src/Python_surf_project.py
@@ -1,44 +1,105 @@
+import os
+
 import requests
 import json
 import datetime
 import yaml
 from datetime import date
-lat1 = 8.4095
-lon = 115.1889
 
+REQUIRED_LATITUDE = 8.4095
+REQUIRED_LONGITUDE = 115.1889
 
 with open('../credentials/configTest.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-key = config['apiWeather']
-key_water = config['apiWaves']
+OPEN_WEATHER_KEY = config['apiWeather']
+STORMGLASS_KEY = config['apiWaves']
 
 
-response_water = requests.get(f"https://api.stormglass.io/v2/weather/point?lat={lat1}&lng={lon}&params=windSpeed"
-                              f"Authorization: {key_water}")
-response = requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat1}&lon={lon}&appid={key}')
-print(response_water.text)
-info = response.json()
-info_water = response_water.json()
-#water_temp = info_water['waterTemperature']
-#print(water_temp)
-print(info_water.keys())
+def search_weather_by_time(response_water, requested_time: str):
+    count = 0
+    for time_period in response_water.json()['hours']:
+        # if time_period['time'] == requested_time:
+        #     print('нашли')
+        print(time_period)
+        count += 1
+    print(count)
 
 
+def get_data_from_stormglass():
+    url_storm_glass = 'https://api.stormglass.io/v2/weather/point'
+    params_storm_glass = {
+        'lat': REQUIRED_LATITUDE,
+        'lng': REQUIRED_LONGITUDE,
+        'params': ['waveHeight', 'wind_speed', 'wavePeriod']
+    }
+    headers_storm_glass = {'Authorization': STORMGLASS_KEY}
 
-wind_speed = info['current']['wind_speed']
-time_sunrise = info['current']['sunrise']
-time_sunset = info['current']['sunset']
-sunrise = datetime.datetime.fromtimestamp(time_sunrise)
-sunset = datetime.datetime.fromtimestamp(time_sunset)
-current_time = datetime.datetime.now()
-if wind_speed > 4.00:
-    print(f'Time sunrise is {sunrise.time()}, time sunset is {sunset.time()}, wind is so strong for surfing {wind_speed}')
-else:
-    print(f'Time sunrise is {sunrise.time()}, time sunset is {sunset.time()}, wind speed is good for surfing {wind_speed}')
-current_time1 = str(current_time.time())
-print(f'Current time is {current_time1[:8]}')
-if sunrise < current_time < sunset :
-    print('Go to surf')
-else:
-    print('Too dark')
+    if os.path.isdir('../static'):
+        if os.path.isfile('../static/StormGlassData.json'):
+            print('reading from local file')
+        else:
+            response_storm_glass = requests.get(
+                url=url_storm_glass,
+                params=params_storm_glass,
+                headers=headers_storm_glass
+            )
+
+            with open('../static/StormGlassData.json', 'w+') as json_file:
+                json.dump(response_storm_glass.json(), json_file, indent=4)
+    else:
+        os.mkdir('../static')
+        response_storm_glass = requests.get(
+            url=url_storm_glass,
+            params=params_storm_glass,
+            headers=headers_storm_glass
+        )
+
+        with open('../static/StormGlassData.json', 'w+') as json_file:
+            json.dump(response_storm_glass.json(), json_file, indent=4)
+    return 0
+
+
+def get_data_from_openweathermap():
+    url_openweather = 'https://api.openweathermap.org/data/3.0/onecall'
+    params_openweather = {'lat': REQUIRED_LATITUDE, 'lon': REQUIRED_LONGITUDE, 'appid': OPEN_WEATHER_KEY}
+    response = requests.get(
+        url=url_openweather,
+        params=params_openweather
+    )
+    return response
+
+
+if __name__ == '__main__':
+
+    print(get_data_from_openweathermap().text)
+    print(get_data_from_stormglass())
+
+    # requested_time = '2022-06-21T00:00:00+00:00'
+
+    # response = requests.get(f'https://api.openweathermap.org/data/3.0/onecall?lat={lat1}&lon={lon}&appid={key}')
+    # print(response_water.text)
+    # info = response.json()
+    # info_water = response_water.json()
+    # #water_temp = info_water['waterTemperature']
+    # #print(water_temp)
+    # print(info_water.keys())
+    #
+    #
+    #
+    # wind_speed = info['current']['wind_speed']
+    # time_sunrise = info['current']['sunrise']
+    # time_sunset = info['current']['sunset']
+    # sunrise = datetime.datetime.fromtimestamp(time_sunrise)
+    # sunset = datetime.datetime.fromtimestamp(time_sunset)
+    # current_time = datetime.datetime.now()
+    # if wind_speed > 4.00:
+    #     print(f'Time sunrise is {sunrise.time()}, time sunset is {sunset.time()}, wind is so strong for surfing {wind_speed}')
+    # else:
+    #     print(f'Time sunrise is {sunrise.time()}, time sunset is {sunset.time()}, wind speed is good for surfing {wind_speed}')
+    # current_time1 = str(current_time.time())
+    # print(f'Current time is {current_time1[:8]}')
+    # if sunrise < current_time < sunset :
+    #     print('Go to surf')
+    # else:
+    #     print('Too dark')
